da5c164c5e357c4017bcedc21b8efa626a4e7aa6
victormoraes <victor.moraesrj@gmail.com>
2024-05-07 09:44:17 -0300

    Styling using LightColorizedStyle, breaking default dark themes

--- plotting.py
+++ plotting.py
@@ -1,10 +1,11 @@
 import pygal 
-from pygal.style import RotateStyle, LightColorizedStyle
+from pygal.style import RotateStyle as RS, LightColorizedStyle as LCS
 from world_population import world_pop_less_10m, world_pop_less_1b, world_pop_over_1b
 
 
+# LightColorizedStyle  lighten the style of charts
 
-wm_style = RotateStyle('#336699')
+wm_style = RS('#4CF5E3', base_style=LCS)
 wm = pygal.maps.world.World(style=wm_style)
 wm.title = 'População Mundial em 2010, por país'
 
@@ -12,4 +13,4 @@ wm.add('População inferior a 10 milhões de habitantes', world_pop_less_10m)
 wm.add('População inferior a 1 bilhão de habitantes', world_pop_less_1b)
 wm.add('População superior a 1 bilhão de habitantes', world_pop_over_1b)
 
-wm.render_to_file('examples/world_pop_styled.svg')
+wm.render_to_file('examples/world_pop_styled_light.svg')
